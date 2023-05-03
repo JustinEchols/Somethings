47c100ae50981195022e4ef24abb5d305fa2446a
Justin Echols <justin.echols@smoothstack.com>
2023-05-03 10:15:23 -0500

    added helper macros to base.h

--- src/base.h
+++ src/base.h
@@ -87,8 +87,9 @@
 # error Context unresolved for this compiler
 #endif
 
+//NOTE(Justin):  
 #if !defined(COMPILER_CL)
-# define COMPILER_CL 0
+# define COMPILER _CL 0
 #endif
 #if !defined(COMPILER_CLANG)
 # define COMPILER_CLANG 0
@@ -118,5 +119,67 @@
 # define ARCH_ARM64 0
 #endif
 
+//Note(Justin): Helper Macros.
+
+#define Statement(S) do{ S }while(0)
+
+#if !defined(AssertBreak)
+# define AssertBreak() (*(volatile int *)0 = 0)
+#endif
+
+#if ENABLE_ASSERT
+# define Assert(c) Statement( if (!(c)){ AssertBreak(); } )
+#else
+# define Assert(c)
+#endif
+
+#define Stringify_(S) #S
+#define Stringify(S) Stringify_(S)
+#define Glue_(A, B) A##B
+#define Glue(A, B) Glue_(A, B)
+
+#define ArrayCount(a) (sizeof(a) / sizeof(*(a)))
+
+#define IntFromPtr(p) (unsigned long long)((char *)p - (char *)0)
+#define PtrFromInt(n) (void *)((char *)0 + (n))
+
+//NOTE(Justin): This Member macro can access a member of a struct. However
+// we cannot R/W to the member. Useful for getting offsets.
+
+#define Member(T,m) (((T *)0)->m)
+#define OffsetOfMember(T,m) IntFromPtr(&Member(T,m))
+
+#define Min(a,b) (((a) < (b)) ? (a) : (b))
+#define Max(a,b) (((a) > (b)) ? (a) : (b))
+
+#define Clamp(a, x, b) (((x) < (a)) ? (a):\
+						((b) < (x)) ? (b) : (x))
+
+#define ClampTop(a,b) Min(a,b)
+#define ClampBot(a,b) Max(a,b)
+
+#define global static
+#define local static
+#define function static
+
+#define c_linkage_begin extern "C"{
+#define c_linkage_end }
+#define c_linkage extern "C"
+ 
+#include <string.h>
+#define MemoryZero(p,z) memset((p), 0, (z))
+#define MemoryZeroStruct(p) MemoryZero((p), sizeof(*(p)))
+#define MemoryZeroArray(p) MemoryZero((p), sizeof(p))
+#define MemoryZeroTyped(p,c) MemoryZero((p), sizeof(*(p))*(c))
+
+#define MemoryMatch(a,b,z) (memcmp((a),(b),(z)) == 0)
+
+#define MemoryCopy(d, s, z) memmove((d), (s), (z))
+#define MemoryCopyStruct(d,s) MemoryCopy((d),(s),\
+											Min(sizeof(*(d)), sizeof(*(s)))
+
+#define MemoryCopyArray(d, s) MemoryCopy((d), (s), Min(sizeof(d), sizeof(s)))
+#define MemoryCopyTyped(d, s, c) MemoryCopy((d), (s),\
+												Min(sizeof(*(d)), sizeof(*(s)))*(c))
 #define BASE_H
 #endif

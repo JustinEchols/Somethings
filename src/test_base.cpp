8276af4b726d4a99c92c5e17548348a89983b648
Justin Echols <justin.echols@smoothstack.com>
2023-05-05 07:51:49 -0500

    updated to include new tests of utilities

--- src/test_base.cpp
+++ src/test_base.cpp
@@ -1,6 +1,23 @@
-#include "base.h"
+//***********************
+//
+// Author:	Justin Echols
+// Date LM: 5/5/2023
+// License: MIT
+//
+//***********************
 
-#define StdPrint(x) printf("%s = %d\n", #x, (int)(x))
+// This file is intended for simple checks of the utility/base layer. The base
+// layer will be essentially used everywhere, so it will get tested throughout
+// development. Which is why we are only doing simple checks ATM.
+
+#include "base.h"
+#include "base.cpp"
+
+#define StdPrint(x) printf("%s = %d\n", #x, (s32)(x))
+#define StdPrintLL(x) printf("%s = %lld\n", #x, (s64)(x))
+#define StdPrintU(x) printf("%s = %u\n", #x, (u32)(x))
+#define StdPrintULL(x) printf("%s = %llu\n", #x, (u64)(x))
+#define StdPrintF(x) printf("%s = %f\n", #x, (f32)(x))
 
 struct TestStruct
 {
@@ -23,13 +40,20 @@ int main()
 	MemoryCopyArray(bar, foo);
 	StdPrint(bar[50]);
 	StdPrint(MemoryMatch(foo, bar, sizeof(foo)));
+
 	MemoryZeroArray(bar);
 	StdPrint(bar[50]);
 	StdPrint(MemoryMatch(foo, bar, sizeof(foo)));
 
+	StdPrint(OffsetOfMember(TestStruct, a));
+	StdPrint(OffsetOfMember(TestStruct, b));
+	StdPrint(OffsetOfMember(TestStruct, c));
+	StdPrint(OffsetOfMember(TestStruct, d));
+
 	TestStruct t = {1, 2, 3, 4};
 	StdPrint(t.a);
 	StdPrint(t.d);
+
 	MemoryZeroStruct(&t);
 	StdPrint(t.a);
 	StdPrint(t.d);
@@ -42,17 +66,20 @@ int main()
 	StdPrint(Clamp(1, 0, 100));
 	StdPrint(Clamp(1, 500, 100));
 
-#if 0
-	printf("cl		= %d\n", COMPILER_CL);
-	printf("clang	= %d\n", COMPILER_CLANG);
-	printf("gcc		= %d\n", COMPILER_GCC);
-	printf("win		= %d\n", OS_WINDOWS);
-	printf("lin		= %d\n", OS_LINUX);
-	printf("mac		= %d\n", OS_MAC);
-	printf("x64		= %d\n", ARCH_X64);
-	printf("x86		= %d\n", ARCH_X86);
-	printf("arm		= %d\n", ARCH_ARM);
-	printf("arm64	= %d\n", ARCH_ARM64);
-#endif
+	StdPrint(min_s8);
+	StdPrint(min_s16);
+	StdPrint(min_s32);
+	StdPrintLL(min_s64);
+
+	StdPrint(max_s8);
+	StdPrint(max_s16);
+	StdPrint(max_s32);
+	StdPrintLL(max_s64);
+
+	f32 inf = inf_f32();
+	f32 neg_inf = neg_inf_f32();
+	StdPrintF(inf);
+	StdPrintF(neg_inf);
+
 	return(0);
 }
